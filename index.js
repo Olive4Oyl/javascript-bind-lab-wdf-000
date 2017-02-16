2a9a05aaccfe6aa40e0c096866c4667b038d5a73
Kelene Chung <simoy93@gmail.com>
2017-02-16 14:54:22 -0500

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+ time: Math.round(Date.now() / 1000),
+  startTicking() {
+    setInterval(function(){
+      this.time++
+    }.bind(this), 1000)
+  }
+}
+
+function censor(word, string) {
+  while(string.includes(word)) {
+    string = string.replace(word, "BLEEP");
+  }
+  return string;
+};
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor  = censor.bind(null, 'drugs');
